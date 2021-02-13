0e74daaa3491fa46335b02bac12a3992294ec6b7
Ashrith Jain <ashrith.jain@robosoftin.com>
2021-02-13 08:05:13 +0530

    ProjectFileCommit

---
+++ PhotosAndVideosApp/PhotosAndVideosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotosAndVideosApp
+//
+//  Created by Ashrith Jain on 13/02/21.
+//  Copyright © 2021 Ashrith Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
